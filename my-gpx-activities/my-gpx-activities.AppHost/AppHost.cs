2b1090cf1129595f6dd6e0b3fa48d43c30325b8d
Frank Boucher <fboucher@outlook.com>
2026-01-31 15:29:44 -0500

    Adds persistent data volume to Postgres
    Configures the Postgres database with a persistent data volume.
    This ensures data is preserved across restarts, supporting reliable data storage for the application.

--- my-gpx-activities/my-gpx-activities.AppHost/AppHost.cs
+++ my-gpx-activities/my-gpx-activities.AppHost/AppHost.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
     .WithPgAdmin()
     .AddDatabase("gpxactivities");
 
